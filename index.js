713b7edac53415cf8a624e37b50574063f7ea4bf
tanjirou-tk <otavioschimith124@gmail.com>
2024-06-22 20:39:53 -0300

    mais próximo que cheguei de dar certo

--- index.js
+++ index.js
@@ -1,52 +1,73 @@
-class Node {
-  //Node references
-  constructor(...index) {
-    this.bonds = index
-  }
+class Cell {
+	/**
+	 * @type {number[]} Cell references
+	 */
+	bonds
+	/**
+	 * Cell references
+	 * @param  {...number} index 
+	 */
+	constructor(...index) {
+		this.bonds = index
+	}
 }
 
-const nodes = [
-  new Node(1, 5),
-  new Node(2, 0),
-  new Node(3, 1),
-  new Node(4, 2),
-  new Node(5, 3),
-  new Node(0, 4),
+const cells = [
+	// new Cell(1, 5),
+	// new Cell(2, 0),
+	// new Cell(3, 1),
+	// new Cell(4, 2),
+	// new Cell(5, 3),
+	// new Cell(0, 4),
 
-  new Node(7),
-  new Node(8, 6),
-  new Node(6, 7), 
-
-  new Node(10),
-  new Node(11, 9),
-  new Node(9, 10)
+	new Cell(1, 4),
+	new Cell(2, 0),
+	new Cell(3, 1),
+	new Cell(4, 2),
+	new Cell(0, 3),
+	
 ]
 const connections = 6
 
+let date = Date.now()
 let qnt = 0
-for (let i = 0; i < nodes.length; i++) {
-  if (check(nodes[i])) qnt++
+for (let i = 0; i < cells.length; i++) {
+	if (check(cells[i])) qnt++
 }
-console.log(qnt, Math.floor(qnt/connections))
+console.log(qnt, Math.floor(qnt / connections), Date.now() - date + "ms")
 
-function check(node) {
-  if (!node.bonds.length) return
-  let array = [...node.bonds]
-  let tempArray = []
-  let previous = nodes.indexOf(node)
-  let previousArray = [array.toString()]
+/**
+ * @param {Cell} cell 
+ */
+function check(cell) {
+	if (!cell.bonds.length) return
 
-  for (let _ = 0; _ < connections - 1; _++) {
-    console.log(_, nodes.indexOf(node), array)
-    for (let i = 0; i < array.length; i++) {
-        let bonds = nodes[array[i]].bonds.filter(e => e != previous)
-        tempArray.push(...bonds)
-    }
-    previous = array[_]
-    array = [...tempArray]
-    if (previousArray.includes(array.toString())) return false
-    previousArray.push(array.toString())
-    tempArray = []
-  }
-  if (array.includes(nodes.indexOf(node))) return true
+	let flow = [[[cells.indexOf(cell)]], [cell.bonds]]
+	if (cells[flow[1][0][0]].bonds.includes(flow[0][0][0]) && connections == 2) return true
+
+	for (let i = 2; i < connections + 1; i++) {
+		flow[i] = []
+	}
+
+	for (let x = 1; x < flow.length - 1; x++) {
+		let zcounter = 0
+
+		if (flow[x].length == 0) return false
+		if ((connections % x == 0) && flow[x].reduce((p, c) => p.concat(c)).includes(cells.indexOf(cell))) return false
+
+		for (let y = 0; y < flow[x].length; y++) {
+			for (let z = 0; z < flow[x][y].length; z++) {
+
+				const ycounter = zcounter
+				const cell = cells[flow[x][y][z]]
+				flow[x+1][zcounter] = []
+				cell.bonds.filter(c => c != flow[x - 1][Math.floor(y/2)][Math.floor(y/2)]).forEach((c, i) => {
+					flow[x+1][ycounter][i] = c
+				})
+				zcounter++
+			}
+		}
+	}
+	console.log(flow)
+	if (flow[connections].length && flow[connections].reduce((p, c) => p.concat(c)).includes(cells.indexOf(cell))) return true
 }
