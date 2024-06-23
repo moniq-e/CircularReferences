be547ea6c1d1053281e70d8cfa37fa5ee3256610
tanjirou-tk <otavioschimith124@gmail.com>
2024-06-23 01:16:57 -0300

    conflict solved

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-class Node {
-    //Node references
+class Cell {
+    //Cell references
     constructor(...index) {
         this.bonds = index
     }
 }
 
-const nodes = [
-    new Node(1),
-    new Node(2),
-    new Node(3),
-    new Node(4),
-    new Node(5),
-    new Node(0, 6),
-    new Node()
+const cells = [
+    new Cell(1),
+    new Cell(2),
+    new Cell(3),
+    new Cell(4),
+    new Cell(5),
+    new Cell(0, 6),
+    new Cell()
 ]
 const connections = 6
 
 let qnt = 0
-for (let i = 0; i < nodes.length; i++) {
-    if (check(nodes[i])) qnt++
+for (let i = 0; i < cells.length; i++) {
+    if (check(cells[i])) qnt++
 }
 console.log(Math.floor(qnt/connections))
 
-function check(node) {
-    if (!node.bonds.length) return
-    let array = [...node.bonds]
+function check(cell) {
+    if (!cell.bonds.length) return
+    let array = [...cell.bonds]
     let tempArray = []
     for (let _ = 0; _ < connections - 1; _++) {
         for (let i = 0; i < array.length; i++) {
-            tempArray.push(...nodes[array[i]].bonds)
+            tempArray.push(...cells[array[i]].bonds)
         }
         array = [...tempArray]
         tempArray = []
     }
-    if (array.includes(nodes.indexOf(node))) return true
+    if (array.includes(cells.indexOf(cell))) return true
 }
