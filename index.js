e205d9a8871b1a97ef9136f1fc896753329ae9fa
tanjirou-tk <otavioschimith124@gmail.com>
2024-06-23 01:20:59 -0300

    funcionando, special thanks to timidtlk!
    Co-authored-by: timidtlk <timidtlk@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,72 +1,43 @@
 class Cell {
-	/**
-	 * @type {number[]} Cell references
-	 */
-	bonds
-	/**
-	 * Cell references
-	 * @param  {...number} index 
-	 */
-	constructor(...index) {
-		this.bonds = index
-	}
+    //Cell references
+    constructor(...index) {
+        this.bonds = index
+    }
 }
 
 const cells = [
-	// new Cell(1, 5),
-	// new Cell(2, 0),
-	// new Cell(3, 1),
-	// new Cell(4, 2),
-	// new Cell(5, 3),
-	// new Cell(0, 4),
-
-	new Cell(1, 4),
-	new Cell(2, 0),
-	new Cell(3, 1),
-	new Cell(4, 2),
-	new Cell(0, 3),
-	
+    new Cell(1, 5),
+    new Cell(2, 0),
+    new Cell(3, 1),
+    new Cell(4, 2),
+    new Cell(5, 3),
+    new Cell(0, 4)
 ]
 const connections = 6
-
-let date = Date.now()
-let qnt = 0
-for (let i = 0; i < cells.length; i++) {
-	if (check(cells[i])) qnt++
-}
-console.log(qnt, Math.floor(qnt / connections), Date.now() - date + "ms")
-
 /**
- * @param {Cell} cell 
+ * @type {number[][]}
  */
-function check(cell) {
-	if (!cell.bonds.length) return
-
-	let flow = [[[cells.indexOf(cell)]], [cell.bonds]]
-	if (cells[flow[1][0][0]].bonds.includes(flow[0][0][0]) && connections == 2) return true
-
-	for (let i = 2; i < connections + 1; i++) {
-		flow[i] = []
+const bonds = []
+for (let i = 0; i < cells.length; i++) {
+	const cell = cells[i]
+	for (const bonded of cell.bonds) {
+		if (!bonds.find(b => b[0] == bonded && b[1] == i)) bonds.push([i, bonded])
 	}
+}
 
-	for (let x = 1; x < flow.length - 1; x++) {
-		let zcounter = 0
+let qnt = 0
+for (let i = 0; i < bonds.length; i++) {
+    if (check(bonds[i])) qnt++
+}
+console.log(qnt)
 
-		if (flow[x].length == 0) return false
-		if ((connections % x == 0) && flow[x].reduce((p, c) => p.concat(c)).includes(cells.indexOf(cell))) return false
-
-		for (let y = 0; y < flow[x].length; y++) {
-			for (let z = 0; z < flow[x][y].length; z++) {
-
-				const ycounter = zcounter
-				const cell = cells[flow[x][y][z]]
-				flow[x+1][zcounter] = []
-				cell.bonds.filter(c => c != flow[x - 1][Math.floor(y/2)][Math.floor(y/2)]).forEach((c, i) => {
-					flow[x+1][ycounter][i] = c
-				})
-				zcounter++
-			}
-		}
+function check(initBond) {
+	let bond = initBond
+	for (let iterations = 0; iterations < (connections - 2); iterations++) {
+		let find = bonds.find(b => b[0] == bond[1])
+		if (find) {
+			bond = find
+		} else return false
 	}
-	if (flow[connections].length && flow[connections].reduce((p, c) => p.concat(c)).includes(cells.indexOf(cell))) return true
+	if (bonds.find(b => b[0] == initBond[0] && b[1] == bond[1]) && initBond != bond) return true
 }
